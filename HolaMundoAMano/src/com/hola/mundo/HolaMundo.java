64f0a9481dc4f3f32f61f8a92c69eb132388957f
leandro.e.molina <leandro.e.molina@49b8205c-dfab-3dba-cd8f-fea7b348a580>
2012-05-20 21:07:15 +0000

    Initial import.

---
+++ HolaMundoAMano/src/com/hola/mundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.hola.mundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HolaMundo extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
